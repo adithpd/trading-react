fa47d7807e8bd304ce4b0b233506b0b7bf6e40e3
adithpd <prithvik02@gmail.com>
2025-01-16 00:16:03 +0530

    Wallet Development ongoing

--- src/page/Activity/Activity.jsx
+++ src/page/Activity/Activity.jsx
@@ -1,7 +1,56 @@
 import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar"
+import bitcoinLogo from "@/assets/icons_custom/bitcoin.svg"
+
 
 export const Activity = () => {
   return (
-    <div>activity</div>
+    <div className="p-5 lg:px-20">
+      <h1 className="font-bold text-3xl pb-5">Activity</h1>
+      <Table className="border">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="py-5">Date & Time</TableHead>
+            <TableHead>Trading Pair</TableHead>
+            <TableHead>Buy Price</TableHead>
+            <TableHead>Sell Price</TableHead>
+            <TableHead>Order Type</TableHead>
+            <TableHead className="">Profit/Loss</TableHead>
+            <TableHead className="text-right">Value</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item, index) =>
+            <TableRow key={index}>
+              <TableCell>
+                  <p>2024/05/31</p>
+                  <p className="text-gray-400">12:39:32</p>
+              </TableCell>
+              <TableCell className="font-medium flex items-center gap-2">
+                <Avatar className="-z-50">
+                  <AvatarImage src={bitcoinLogo} className="h-8 w-8" />
+                </Avatar>
+                <span>BitCoin</span>
+              </TableCell>
+              <TableCell>$150</TableCell>
+              <TableCell className="">$250.00</TableCell>
+              <TableCell>BUY</TableCell>
+              <TableCell>+1002</TableCell>
+              <TableCell className="text-right">
+                345
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
   )
 }
--- src/page/Wallet/Wallet.jsx
+++ src/page/Wallet/Wallet.jsx
@@ -1,7 +1,60 @@
 import React from 'react'
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { CopyIcon, DollarSign, UploadIcon, WalletIcon } from 'lucide-react'
+import { ReloadIcon } from '@radix-ui/react-icons'
+import { TopupForm } from './TopupForm';
 
 export const Wallet = () => {
   return (
-    <div>wallet</div>
+    <div className="flex flex-col items-center">
+      <div className="pt-10 w-full lg:w-[60%]">
+        <Card>
+          <CardHeader className="pb-9">
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-5">
+                <WalletIcon size={30}/>
+                <div>
+                  <CardTitle className="text-2xl">My Wallet</CardTitle>
+                  <div className="flex items-center gap-2">
+                    <p className="text-gray-200 text-sm">#A475Ed</p>
+                    <CopyIcon size={12} className="cursor-pointer hover:text-slate-300"/>
+                  </div>
+              </div>
+              </div>
+              <div>
+                <ReloadIcon className="w-6 h-6 cursor-pointer hover:text-gray-400"/>
+              </div>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <div className="flex items-center">
+              <DollarSign/>
+              <span className="text-2xl font-semibold">
+                20000
+              </span>
+            </div>
+            <div className="flex gap-7 mt-5">
+              <Dialog>
+                <DialogTrigger>
+                  <div className="h-24 w-24 hover:text-gray-400 cursor-pointer flex flex-col items-center justify-center rounded-md shadow-slate-800 shadow-md">
+                    <UploadIcon/>
+                    <span className="text-sm mt-2">Add Money</span>
+                  </div>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>
+                      Top Up Your Wallet
+                    </DialogTitle>
+                  </DialogHeader>
+                  <TopupForm/>
+                </DialogContent>
+              </Dialog>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   )
 }
