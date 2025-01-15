141e5a3385fbd58f55d0bcfb1f94750dbac8c5f9
adithpd <prithvik02@gmail.com>
2025-01-15 23:32:05 +0530

    working on portfolio and watchlist

--- src/page/Portfolio/Portfolio.jsx
+++ src/page/Portfolio/Portfolio.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar"
+import bitcoinLogo from "@/assets/icons_custom/bitcoin.svg"
+
 
 export const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div className="p-5 lg:px-20">
+      <h1 className="font-bold text-3xl pb-5">Portfolio</h1>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="">Asset</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Unit</TableHead>
+            <TableHead>Change</TableHead>
+            <TableHead>Change%</TableHead>
+            <TableHead className="text-right">Volume</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item, index) =>
+            <TableRow key={index}>
+              <TableCell className="font-medium flex items-center gap-2">
+                <Avatar className="-z-50">
+                  <AvatarImage src={bitcoinLogo} className="h-8 w-8" />
+                </Avatar>
+                <span>BitCoin</span>
+              </TableCell>
+              <TableCell>BTC</TableCell>
+              <TableCell>1010102022202</TableCell>
+              <TableCell>1010102022202</TableCell>
+              <TableCell>1010102022202</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
   )
 }
--- src/page/Watchlist/Watchlist.jsx
+++ src/page/Watchlist/Watchlist.jsx
@@ -1,7 +1,61 @@
 import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar"
+import bitcoinLogo from "@/assets/icons_custom/bitcoin.svg"
+import { Button } from '@/components/ui/button';
+import { BookmarkFilledIcon } from '@radix-ui/react-icons';
+
 
 export const Watchlist = () => {
+  const handleRemoveToWatchlist=(value)=>{
+    console.log(value);
+  }
+
   return (
-    <div>watchlist</div>
+    <div className="p-5 lg:px-20">
+      <h1 className="font-bold text-3xl pb-5">Watchlist</h1>
+      <Table className="border">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="py-5">Coin</TableHead>
+            <TableHead>Symbol</TableHead>
+            <TableHead>Volume</TableHead>
+            <TableHead>Market Cap</TableHead>
+            <TableHead>24h</TableHead>
+            <TableHead className="">Price</TableHead>
+            <TableHead className="text-right text-red-600">Remove</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item, index) =>
+            <TableRow key={index}>
+              <TableCell className="font-medium flex items-center gap-2">
+                <Avatar className="-z-50">
+                  <AvatarImage src={bitcoinLogo} className="h-8 w-8" />
+                </Avatar>
+                <span>BitCoin</span>
+              </TableCell>
+              <TableCell>BTC</TableCell>
+              <TableCell>1010102022202</TableCell>
+              <TableCell>1010102022202</TableCell>
+              <TableCell>1010102022202</TableCell>
+              <TableCell className="">$250.00</TableCell>
+              <TableCell className="text-right">
+                <Button variant="ghost" onClick={() => handleRemoveToWatchlist(item.id)} size="icon" className="h-10 w-10">
+                    <BookmarkFilledIcon className="h-6 w-6"/>
+                </Button>
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
   )
 }
