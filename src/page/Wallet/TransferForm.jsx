b7c049c420059a4ebceb976ad763131bffcfe884
adithpd <prithvik02@gmail.com>
2025-01-18 14:24:10 +0530

    minor development

--- src/page/Wallet/TransferForm.jsx
+++ src/page/Wallet/TransferForm.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { DialogClose } from "@/components/ui/dialog"
+
 
 export const TransferForm = () => {
+  const [formData, setFormData] = React.useState({
+    amount: '',
+    walletId: '',
+    purpose: '',
+  })
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value});
+  }
+
+  const handleSubmit = () => {
+    console.log(formData);
+  }
+
   return (
-    <div>TransferForm</div>
+    <div className="pt-10 space-y-5">
+      <div>
+        <h1 className="pb-1">Enter Amount</h1>
+        <Input name="amount" onChange={handleChange} value={formData.amount} className="py-7" placeholder="$9999"/>
+      </div>
+
+      <div>
+        <h1 className="pb-1">Wallet Id</h1>
+        <Input name="walletId" onChange={handleChange} value={formData.walletId} className="py-7" placeholder="#ADER455"/>
+      </div>
+
+      <div>
+        <h1 className="pb-1">Purpose</h1>
+        <Input name="purpose" onChange={handleChange} value={formData.purpose} className="py-7" placeholder="Gift for Friend"/>
+      </div>
+
+      <DialogClose className="w-full">
+          <Button onClick={handleSubmit} className="w-full py-7">
+            Submit
+          </Button>
+      </DialogClose>
+    </div>
   )
 }
